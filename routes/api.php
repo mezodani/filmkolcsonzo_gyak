889855588a0d45c5b304021ee21a20f9ba208ebf
mezodani <mezodani06@gmail.com>
2026-03-18 18:37:46 +0100

    api.php routes done

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\RentalController;
+use App\Models\Rental;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/films', [FilmController::class, 'index']);
+Route::post('/films', [FilmController::class, 'store']);
+
+Route::get('/customers', [CustomerController::class, 'index']);
+Route::post('/customers', [CustomerController::class, 'store']);
+
+Route::get('/rentals', [RentalController::class, 'index']);
+Route::post('/rentals', [RentalController::class, 'store']);
+Route::put('/rentals', [RentalController::class, 'update']);
+Route::delete('/rentals', [RentalController::class, 'destroy']);
